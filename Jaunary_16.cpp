bb22202f03b8d6132a7542a80b3b0ce6d7465900
Shresth Deorari <shresthdeorari38@gmail.com>
2025-01-16 10:39:19 +0530

    Add files via upload

---
+++ Jaunary_16.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int ans = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+        if(n&1){
+            for(auto i : nums2){
+                ans ^= i;
+            }
+        }
+        if(m&1){
+            for(auto i : nums1){
+                ans ^=i;
+            }
+        }
+        return ans;
+    }
+};
